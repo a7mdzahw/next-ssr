c3c460293dd66074bebaab61e5618c33844c6271
a7mdzahw <a7mdzahw@gmail.com>
2021-04-08 16:55:57 +0200

    fixed link import

--- components/signup/Step2Form.js
+++ components/signup/Step2Form.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/Link";
+import Link from "next/link";
 import getApiError from "../../lib/getApiError";
 import getError from "../../lib/getError";
 
