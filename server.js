d88232993b4863a67313508bbf615925b6293565
a7mdzahw <a7mdzahw@gmail.com>
2021-04-08 16:58:59 +0200

    allow more than one port

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app
       return handle(req, res);
     });
     // server listening
-    server.listen(3000, (err) => {
+    server.listen(process.env.PORT || 3000, (err) => {
       if (err) throw err;
       console.log("> Ready on http://localhost:3000");
     });
